4b6fc3618ade703b38312da0be79bfb49ce0fa8b
Daniel Marmelo <daniel.marmelo@hotmail.com>
2020-04-16 00:49:27 +0100

    Api new endpoints, security and refactoring.

--- src/main/java/dev/estgp/is/api/Api.java
+++ src/main/java/dev/estgp/is/api/Api.java
@@ -3,6 +3,7 @@ package dev.estgp.is.api;
 import com.google.gson.Gson;
 import dev.estgp.is.Application;
 import dev.estgp.is.models.Customer;
+import dev.estgp.is.models.Invoice;
 import dev.estgp.is.models.User;
 
 import static spark.Spark.*;
@@ -14,13 +15,14 @@ public class Api {
 
         // API endpoints
         path("/api", () -> {
-            //before("/*", (q, a) -> log.info("Received api call"));
-
+            // TODO omitir password e apiKey
+            // TODO Utility to remove
             get("/users/", (request, response) -> {
                 response.type("application/json");
                 return gson.toJson(User.all());
             });
 
+            // TODO Utility to remove
             get("/users/:id/", (request, response) -> {
                 response.type("application/json");
                 String id = request.params(":id");
@@ -33,8 +35,9 @@ public class Api {
                 }
             });
 
-
+            // Intercept all api requests to verify authentication
             before("/*", (request, response) -> {
+                // Only let the login pass
                 if (request.pathInfo().startsWith("/api/user")) {
                     return;
                 }
@@ -73,26 +76,13 @@ public class Api {
                 return "";
             });
 
+            // Returns a list of all the logged user customers
             get("/customers/", (request, response) -> {
-                // Retornar a lista de todos os meus clientes
                 User user = request.attribute(Application.USER_KEY);
                 return gson.toJson(Customer.get(user));
             });
 
-            get("/customers/:id/", (request, response) -> {
-                User user = request.attribute(Application.USER_KEY);
-                String customerId = request.params(":id");
-                Customer customer = Customer.get(Integer.parseInt(customerId));
-                if (customer != null) {
-                    if (customer.user_id == user.id) {
-                        return gson.toJson(customer);
-                    } else
-                        response.status(403);
-                } else
-                    response.status(404);
-                return "";
-            });
-
+            // Creates a new Customer to the logged user
             post("/customers/", (request, response) -> {
                 User user = request.attribute(Application.USER_KEY);
                 Customer customer = gson.fromJson(request.body(), Customer.class);
@@ -102,27 +92,127 @@ public class Api {
                 return gson.toJson(customer);
             });
 
-            delete("/customers/:id/", (request, response) -> {
+            // Intercept all requests to customers to verify if the customer id is one of the logged user
+            before("/customers/:id/*", (request, response) -> {
                 User user = request.attribute(Application.USER_KEY);
-                String customerId = request.params(":id");
-                Customer customer = Customer.get(Integer.parseInt(customerId));
-                if (customer != null) {
-                    if (customer.user_id == user.id) {
-                        customer.delete();
-                    } else {
-                        halt(403);
-                    }
-                } else {
-                    halt(404);
+                String id = request.params(":id");
+                int idInt = 0;
+                try {
+                    idInt = Integer.parseInt(id);
+                } catch (NumberFormatException e) {
+                    Application.sendError(400);
                 }
+                Customer customer = Customer.get(idInt);
+                if (customer == null) {
+                    Application.sendError(404);
+                }
+                else if (customer.user_id != user.id) {
+                    Application.sendError(403);
+                }
+                else {
+                    request.attribute(Application.CUSTOMER_KEY, customer);
+                }
+            });
+
+            // Returns a customer by id
+            get("/customers/:id/", (request, response) -> {
+                Customer customer = request.attribute(Application.CUSTOMER_KEY);
+                return gson.toJson(customer);
+            });
+
+            // Updates a Customer
+            put("/customer/:id/", (request, response) -> {
+                Customer customerDb = request.attribute(Application.CUSTOMER_KEY);
+                Customer customer = gson.fromJson(request.body(), Customer.class);
+                if (customer.name != null && !customer.name.isBlank())
+                    customerDb.name = customer.name;
+                if (customer.email != null && !customer.email.isBlank())
+                    customerDb.email = customer.email;
+                customerDb.save();
+                response.status(200);
+                return gson.toJson(customerDb);
+            });
+
+            // Deletes a Customer
+            delete("/customers/:id/", (request, response) -> {
+                Customer customer = request.attribute(Application.CUSTOMER_KEY);
+                customer.delete();
                 return "";
             });
 
-            // get /api/customers/:id/invoices
-            // get /api/customers/:id/invoices/:invoiceId
-            // before's segurança
+            // Returns a list of all the Invoices of the Customer
+            get("/customers/:id/invoices/", (request, response) -> {
+                Customer customer = request.attribute(Application.CUSTOMER_KEY);
+                return gson.toJson(Invoice.getByCustomer(customer));
+            });
+
+            // Creates a new Invoice for the Customer
+            post("/customers/:id/invoices/", (request, response) -> {
+                Customer customer = request.attribute(Application.CUSTOMER_KEY);
+                Invoice invoice = gson.fromJson(request.body(), Invoice.class);
+                invoice.customer_id = customer.id;
+                invoice.id = customer.save();
+                response.status(201);
+                return gson.toJson(invoice);
+            });
+
+            // TODO get /api/customers/:id/invoices/:invoiceId
+
+            // Intercept all requests to Invoice to verify if the customer id is one of the logged user
+            before("/invoices/:id/*", (request, response) -> {
+                User user = request.attribute(Application.USER_KEY);
+                String id = request.params(":id");
+                int idInt = 0;
+                try {
+                    idInt = Integer.parseInt(id);
+                } catch (NumberFormatException e) {
+                    Application.sendError(400);
+                }
+                Invoice invoice = Invoice.get(idInt);
+                if (invoice == null) {
+                    Application.sendError(404);
+                }
+                else {
+                    Customer customer = Customer.get(invoice.customer_id);
+                    if (customer.user_id != user.id) {
+                        Application.sendError(403);
+                    }
+                    else {
+                        request.attribute(Application.CUSTOMER_KEY, customer);
+                        request.attribute(Application.INVOICE_KEY, invoice);
+                    }
+                }
+            });
+
+            // Returns a Invoice by id
+            get("/invoices/:id/", (request, response) -> {
+                Invoice invoice = request.attribute(Application.INVOICE_KEY);
+                return gson.toJson(invoice);
+            });
+
+            // Updates a Invoice
+            put("/invoices/:id/", (request, response) -> {
+                Invoice invoiceDb = request.attribute(Application.INVOICE_KEY);
+                Invoice invoice = gson.fromJson(request.body(), Invoice.class);
+                if (invoice.date != null && !invoice.date.isBlank())
+                    invoiceDb.date = invoice.date;
+                // TODO Check for 0?
+                invoiceDb.amount = invoice.amount;
+                invoiceDb.save();
+                response.status(200);
+                return gson.toJson(invoiceDb);
+            });
+
+            // Deletes a Invoice
+            delete("/customers/:id/", (request, response) -> {
+                Invoice invoice = request.attribute(Application.INVOICE_KEY);
+                invoice.delete();
+                return "";
+            });
+
+            // TODO Complete Invoice
+            //      Check for security breaches
 
         });
-
     }
 }
